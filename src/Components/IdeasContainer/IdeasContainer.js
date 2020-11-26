eeefaf6a24bf803902dd3b014ed60ec7774d418c
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 15:45:15 -0700

    Changed IdeasContainer to HOC and conditional render

--- src/Components/IdeasContainer/IdeasContainer.js
+++ src/Components/IdeasContainer/IdeasContainer.js
@@ -6,7 +6,7 @@ import './IdeasContainer.css';
 const IdeasContainer = ({ ideas }) => {
 	return (
 		<section className='ideas-container'>
-			<h1>A new idea is here!</h1>
+			{ideas.length > 0 && <h2>Add a new idea to your list!</h2>}
 		</section>
 	);
 };
@@ -15,4 +15,4 @@ const mapStateToProps = state => ({
 	ideas: state.ideas,
 });
 
-export default IdeasContainer;
+export default connect(mapStateToProps)(IdeasContainer);
