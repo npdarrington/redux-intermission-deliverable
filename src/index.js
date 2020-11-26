bb495142b61b8714154f1d66c627a674ec87a268
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 14:49:14 -0700

    Added imports for redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
