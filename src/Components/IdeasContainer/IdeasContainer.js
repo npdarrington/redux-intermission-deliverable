0aa064020d04962d77108268f4ba3954425fc02f
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 15:42:37 -0700

    Added basic return to IdeasContainer

--- src/Components/IdeasContainer/IdeasContainer.js
+++ src/Components/IdeasContainer/IdeasContainer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './IdeasContainer.css';
 
-const IdeasContainer = () => {
-	return <section className='ideas-container'></section>;
+const IdeasContainer = ({ ideas }) => {
+	return (
+		<section className='ideas-container'>
+			<h1>A new idea is here!</h1>
+		</section>
+	);
 };
+
+const mapStateToProps = state => ({
+	ideas: state.ideas,
+});
 
 export default IdeasContainer;
