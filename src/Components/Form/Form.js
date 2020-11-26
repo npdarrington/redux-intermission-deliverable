005a3e4369988fae3b25a4f0ec40dea6c0608a86
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 12:55:38 -0700

    Added onChange events to inputs to update correct state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -16,9 +16,19 @@ function Form() {
 	return (
 		<section className='form'>
 			<label htmlFor='Title'>Title:</label>
-			<input name='title' value={title} type='text' />
+			<input
+				name='title'
+				value={title}
+				type='text'
+				onChange={event => setTitle(event.target.value)}
+			/>
 			<label htmlFor='Idea'>Idea:</label>
-			<input name='title' value={idea} type='text' />
+			<input
+				name='title'
+				value={idea}
+				type='text'
+				onChange={event => setIdea(event.target.value)}
+			/>
 			<button type='button' onClick={validateIdea}>
 				Submit
 			</button>
