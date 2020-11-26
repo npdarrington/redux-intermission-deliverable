7c8a1d57005e4d03771f5e4d9216d70b80eaba94
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 15:38:12 -0700

    Changed Form to HOC and adds new idea on submit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { addIdea } from '../../Redux/Ideas/Ideas.actions';
 import { connect } from 'react-redux';
 
-const Form = props => {
+const Form = ({ addIdea }) => {
 	const [title, setTitle] = useState('');
 	const [idea, setIdea] = useState('');
 	const [error, setError] = useState('');
@@ -12,7 +12,7 @@ const Form = props => {
 			setError('A title and idea must be filled in');
 		} else {
 			const newIdea = { title: title, idea: idea };
-			this.props.addIdea(newIdea);
+			addIdea(newIdea);
 			resetInputs();
 		}
 	};
@@ -51,4 +51,4 @@ const mapDispatchToProps = dispatch => ({
 	addIdea: idea => dispatch(addIdea(idea)),
 });
 
-export default Form;
+export default connect(null, mapDispatchToProps)(Form);
