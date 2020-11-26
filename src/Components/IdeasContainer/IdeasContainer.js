b58837275bbc03ed75ea537b39522d54f79fb0fb
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 15:58:42 -0700

    Map over ideas to display for user, fix conditional render

--- src/Components/IdeasContainer/IdeasContainer.js
+++ src/Components/IdeasContainer/IdeasContainer.js
@@ -6,7 +6,10 @@ import './IdeasContainer.css';
 const IdeasContainer = ({ ideas }) => {
 	return (
 		<section className='ideas-container'>
-			{ideas.length > 0 && <h2>Add a new idea to your list!</h2>}
+			{ideas.length < 1 && <h2>Add a new idea to your list!</h2>}
+			{ideas.map(idea => (
+				<h3>A new idea goes here!</h3>
+			))}
 		</section>
 	);
 };
