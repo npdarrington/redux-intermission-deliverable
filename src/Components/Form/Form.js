73c8ef4dc5e958906639cc2bf552c5b58e5c8954
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 12:37:12 -0700

    Added basic form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 function Form() {
-	return <section className='form'></section>;
+	return (
+		<section className='form'>
+			<label htmlFor='Title'>Title:</label>
+			<input name='title' value={title} type='text' />
+			<label htmlFor='Idea'>Idea:</label>
+			<input name='title' value={idea} type='text' />
+			<button type='button'>Submit</button>
+		</section>
+	);
 }
 export default Form;
