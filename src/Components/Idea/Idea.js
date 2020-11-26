432d237ecaddb72b16d249abf9b70d8dd877f4c3
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 16:00:16 -0700

    Added basic render for testing render

--- src/Components/Idea/Idea.js
+++ src/Components/Idea/Idea.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Idea.css';
 
 const Idea = () => {
-	return <article className='idea-card'></article>;
+	return <article className='idea-card'>A new idea goes here!</article>;
 };
 
 export default Idea;
