891105211109b1bd965e575b659f09e5f66f3793
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 14:55:09 -0700

    Modified ideas reducer to use state properly

--- src/Redux/Ideas/Ideas.reducer.js
+++ src/Redux/Ideas/Ideas.reducer.js
@@ -1,8 +1,9 @@
 export const ideas = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_IDEA':
-			return [...ideas, { id: Date.now(), idea: action.idea, favorite: false }];
+			return [...state, { id: Date.now(), idea: action.idea, favorite: false }];
 
 		default:
+			return state;
 	}
 };
