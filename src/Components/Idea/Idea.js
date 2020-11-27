a7628db7dc272f7eeb759858d7065e97e0444778
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 18:52:47 -0700

    Built idea-card-body changed options to head

--- src/Components/Idea/Idea.js
+++ src/Components/Idea/Idea.js
@@ -5,9 +5,13 @@ import './Idea.css';
 const Idea = ({ id, title, idea }) => {
 	return (
 		<article className='idea-card'>
-			<section className='idea-card-options'>
+			<section className='idea-card-head'>
 				<h2>Delete</h2>
 				<h2>Favorite</h2>
+			</section>
+			<section className='idea-card-body'>
+				<h2>Title goes here</h2>
+				<p>Body goes here</p>
 			</section>
 		</article>
 	);
