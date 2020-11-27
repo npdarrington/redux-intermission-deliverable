ca81fdef1e1785082ebdb2e82ec2250043f536df
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 23:20:48 -0700

    Added favoriting feature with favoriteIdea action

--- src/Components/Idea/Idea.js
+++ src/Components/Idea/Idea.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { deleteIdea } from '../../Redux/Ideas/Ideas.actions';
+import { deleteIdea, favoriteIdea } from '../../Redux/Ideas/Ideas.actions';
 
 import './Idea.css';
 
-const Idea = ({ ideaId, idea, deleteIdea }) => {
+const Idea = ({ ideaId, idea, deleteIdea, favoriteIdea }) => {
 	return (
 		<article className='idea-card'>
 			<section className='idea-card-head'>
 				<h5 onClick={() => deleteIdea(ideaId)}>Delete</h5>
-				<h5>Favorite</h5>
+				<h5 onClick={() => favoriteIdea(ideaId)}>Favorite</h5>
 			</section>
 			<section className='idea-card-body'>
 				<h2>{idea.title}</h2>
@@ -21,6 +21,7 @@ const Idea = ({ ideaId, idea, deleteIdea }) => {
 
 const mapDispatchToProps = dispatch => ({
 	deleteIdea: id => dispatch(deleteIdea(id)),
+	favoriteIdea: id => dispatch(favoriteIdea(id)),
 });
 
 export default connect(null, mapDispatchToProps)(Idea);
