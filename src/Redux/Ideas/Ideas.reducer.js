644a8b71d3bbfa16b728741b363da18b4267e4e0
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 22:47:15 -0700

    Added DELETE_IDEA case

--- src/Redux/Ideas/Ideas.reducer.js
+++ src/Redux/Ideas/Ideas.reducer.js
@@ -2,7 +2,8 @@ export const ideas = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_IDEA':
 			return [...state, { id: Date.now(), idea: action.idea, favorite: false }];
-
+		case 'DELETE_IDEA':
+			return state.filter(idea => idea.id !== action.id);
 		default:
 			return state;
 	}
