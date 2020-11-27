c4cacfff42b89f125b1e6b1206b453508819cae9
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 18:58:04 -0700

    Fixed improperly passing idea

--- src/Components/IdeasContainer/IdeasContainer.js
+++ src/Components/IdeasContainer/IdeasContainer.js
@@ -10,7 +10,7 @@ const IdeasContainer = ({ ideas }) => {
 		<section className='ideas-container'>
 			{ideas.length < 1 && <h2>Add a new idea to your list!</h2>}
 			{ideas.map(idea => (
-				<Idea key={idea.id} idea={idea} />
+				<Idea key={idea.id} idea={idea.idea} />
 			))}
 		</section>
 	);
