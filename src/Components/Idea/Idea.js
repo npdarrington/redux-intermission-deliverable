8deca719d9ac5c613727883f5be87c02b05f45b3
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 22:54:25 -0700

    Modified Idea to HOC to delete an idea when clicking delete

--- src/Components/Idea/Idea.js
+++ src/Components/Idea/Idea.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { deleteIdea } from '../../Redux/Ideas/Ideas.actions';
 
 import './Idea.css';
 
-const Idea = ({ idea: { id, title, idea } }) => {
+const Idea = ({ ideaId, idea, deleteIdea }) => {
 	return (
 		<article className='idea-card'>
 			<section className='idea-card-head'>
-				<h5>Delete</h5>
+				<h5 onClick={() => deleteIdea(ideaId)}>Delete</h5>
 				<h5>Favorite</h5>
 			</section>
 			<section className='idea-card-body'>
-				<h2>{title}</h2>
-				<p>{idea}</p>
+				<h2>{idea.title}</h2>
+				<p>{idea.idea}</p>
 			</section>
 		</article>
 	);
 };
 
-export default Idea;
+const mapDispatchToProps = dispatch => ({
+	deleteIdea: id => dispatch(deleteIdea(id)),
+});
+
+export default connect(null, mapDispatchToProps)(Idea);
