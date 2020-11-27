5d26c95043c6a7381dc549877f6584862d8e8009
Nathan Darrington <npdarrington@icloud.com>
2020-11-26 23:21:10 -0700

    Added FAVORITE_IDEA case

--- src/Redux/Ideas/Ideas.reducer.js
+++ src/Redux/Ideas/Ideas.reducer.js
@@ -4,6 +4,10 @@ export const ideas = (state = [], action) => {
 			return [...state, { id: Date.now(), idea: action.idea, favorite: false }];
 		case 'DELETE_IDEA':
 			return state.filter(idea => idea.id !== action.id);
+		case 'FAVORITE_IDEA':
+			const favoriteIdea = state.find(idea => idea.id === action.id);
+			favoriteIdea.favorite = !favoriteIdea.favorite;
+			return state;
 		default:
 			return state;
 	}
