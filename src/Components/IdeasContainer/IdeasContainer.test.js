c194ed0971010a333913a38ab29d1c97deb8a7d2
Nathan Darrington <npdarrington@icloud.com>
2020-11-28 13:48:05 -0700

    Add mockIdeas

--- src/Components/IdeasContainer/IdeasContainer.test.js
+++ src/Components/IdeasContainer/IdeasContainer.test.js
@@ -8,3 +8,26 @@ import '@testing-library/jest-dom';
 import IdeasContainer from './IdeasContainer';
 
 const store = createStore(rootReducer);
+
+describe('IdeasContainer', () => {
+	test('should render all ideas to the page', () => {
+		const mockIdeas = [
+			{
+				id: 1,
+				idea: {
+					title: 'Test Title 1',
+					idea: 'Test Idea 1',
+				},
+				favorite: false,
+			},
+			{
+				id: 2,
+				idea: {
+					title: 'Test Title 2',
+					idea: 'Test Idea 2',
+				},
+				favorite: false,
+			},
+		];
+	});
+});
