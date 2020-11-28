38c64ac3e914d61bdffa8f3131f575688bd3c85b
Nathan Darrington <npdarrington@icloud.com>
2020-11-28 00:07:50 -0700

    Added additional imports to render Form on screen with sample test

--- src/Components/Form/Form.test.js
+++ src/Components/Form/Form.test.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../Redux/rootReducer';
 import '@testing-library/jest-dom';
 
 import Form from './Form';
+
+const store = createStore(rootReducer);
+
+describe('Form', () => {
+	beforeEach(() => {
+		render(
+			<Provider store={store}>
+				<Form />
+			</Provider>
+		);
+	});
+
+	test('should render the form', () => {
+		screen.debug();
+	});
+});
