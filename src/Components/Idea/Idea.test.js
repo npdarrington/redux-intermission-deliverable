89fa541986481df786006182a646bab1ff3ba736
Nathan Darrington <npdarrington@icloud.com>
2020-11-28 13:46:12 -0700

    Remove unneeded userEvent import

--- src/Components/Idea/Idea.test.js
+++ src/Components/Idea/Idea.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from '../../Redux/rootReducer';
